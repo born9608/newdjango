9a727f93af95e01ed8f8ea33d22257dcdf3747a6
KimBS <born9608@gmail.com>
2023-09-12 13:57:50 +0900

    Setting

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m)r73k$squjg_(h)+hb0beux0fiph%cq^+r4@#-zayxr)s5ky!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.180.43.90']
 
 
 # Application definition
